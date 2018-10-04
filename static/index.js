1ee00de4947582c6b8867752b8a9d57be7d148cf
Håvard Landråk <haavl@stud.ntnu.no>
2018-10-04 15:01:39 +0200

    Lagt til paralax

--- static/index.js
+++ static/index.js
@@ -4,7 +4,8 @@ document.onscroll = function() {navbarChange()};
 
 function navbarChange() {
   var scrollTop = window.pageYOffset;
-
+  var t = scrollTop* (-0.2) - 70;
+  document.getElementById('frontImg').style.top = t+'px';
   if (scrollTop < 500) {
     var trans = scrollTop/500;
     document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,'+trans+')';
