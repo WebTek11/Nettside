169bb37183de1d2e074d5a5ef037e2538b74aa21
Håvard Landråk <haavl@stud.ntnu.no>
2018-10-08 12:44:26 +0200

    NavbarChange.js
    Navbar på fremside skal nå være kombatibel med alle skjermhøyder, med tanke på endring av farge og gjennomsiktighet

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,28 @@ navbarChange()
 
 document.onscroll = function() {navbarChange()};
 
-function navbarChange() {
-  var scrollTop = window.pageYOffset;
-  /*Parallax*/
+function imageParallax() {
   var t = scrollTop* (0.4) - 70;
   document.getElementById('frontImg').style.top = t+'px';
+}
 
-  if (scrollTop < 500) {
-    var trans = scrollTop/500;
-    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,'+trans+')';
+function navbarChange() {
+  var wh = window.innerHeight - 70;
+  var whHalf = wh/2;
+  var scrollTop = window.pageYOffset;
+
+  var navLinks = document.getElementsByClassName('links');
+  if (scrollTop > whHalf && scrollTop < wh) {
+
+    var a = (scrollTop - whHalf)/whHalf;
+
+    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,'+a+')';
     document.getElementById('nav').style.boxShadow = 'none';
     document.getElementById('nav').style.boxShadow = '0px 1px 10px white';
-  }
-  else {
-    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,1)';
-  }
-  if (scrollTop > 500) {
-    document.getElementById('nav').style.boxShadow = '0px 2px 2px grey';
-  }
-  var navLinks = document.getElementsByClassName('links');
-  if (scrollTop < 200) {
+
     const redOri = 19;
     const greenOri = 14;
     const blueOri = 72;
-
-    var a = scrollTop/200;
 
     var redNew = 100 - redOri * a;
     var greenNew = 100 - greenOri * a;
@@ -36,9 +33,18 @@ function navbarChange() {
       navLinks[i].style.color = 'rgb('+redNew+'%,'+greenNew+'%,'+blueNew+'%)';
     }
   }
-  else {
+  else if (scrollTop >= wh) {
+    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,1)';
+    document.getElementById('nav').style.boxShadow = '0px 2px 2px grey';
     for (i = 0; i < navLinks.length; i++) {
       navLinks[i].style.color = 'rgb(81%,86%,26%)';
     }
+  }
+  else {
+    for (i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.color = 'white';
+    }
+    document.getElementById('nav').style.boxShadow = '0px 0px 10px white';
+    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,0)';
   }
 }
