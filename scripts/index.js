11a9e30c890b6fb183d999bcc42dce4878f77c60
Håvard Landråk <haavl@stud.ntnu.no>
2018-11-01 15:15:37 +0100

    Commenting

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
-navbarChange()
+/* Controlles that some code on resize only is done when window goes from being under 960 to over 960.*/
 var wind = window.innerWidth;
+
+navbarChange()
 setBoxHeight()
 controlFunc()
 
 document.onscroll = controlFunc;
 
+/* Function that tells navbarChange and imageParallax not to run on window < 960.
+sets styles for elements on mobile that need this.*/
 function controlFunc() {
   wind = window.innerWidth;
   if (window.innerWidth > 960){
     navbarChange()
     imageParallax()}
   else {
-    document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,0)';
+    document.getElementById('nav').style.backgroundColor = 'rgba(255,255,255,0)';
     document.getElementById('nav').style.boxShadow = 'none';
+    document.getElementById('centerLogo').style.top = '0';
+    document.getElementById('imageBox').style.top = '-83px';
   }
 }
 
-function controlFuncTwo() {
-  if (window.innerWidth <= 960){
-  document.getElementById('centerLogo').style.top = '0';
-  document.getElementById('imageBox').style.top = '-83px';
-  }
-}
-
+/* Makes front page image move together with scrolling, making a parallax effect.*/
 function imageParallax() {
     var scrollTop = window.pageYOffset;
     if (scrollTop < window.innerHeight) {
@@ -34,6 +34,8 @@ function imageParallax() {
     }
 }
 
+/* Sets height for the boxes on the front page such that their height is equal
+to the height of the window minus the navbar.*/
 function setBoxHeight() {
   if (window.innerWidth > 960 && wind > 960){
     var wh = window.innerHeight - 83;
@@ -44,7 +46,7 @@ function setBoxHeight() {
         boxes[i].style.height = '460px';
       }
       for (var i = 0; i < images.length; i++) {
-        images[i].style.height = '360px';
+        images[i].style.height = '330px';
       }
     }
     else {
@@ -61,30 +63,29 @@ function setBoxHeight() {
   }
 }
 
-
+/* Changes the navbars colors smoothly.*/
 function navbarChange() {
   if (window.innerWidth > 960) {
     var wh = window.innerHeight;
     var whHalf = wh/2;
     var scrollTop = window.pageYOffset;
-
     var navLinks = document.getElementsByClassName('links');
 
     if (scrollTop > whHalf && scrollTop < wh) {
-
       var a = (scrollTop - whHalf)/whHalf;
 
+      /* Change the styling of the navbar and make it go from transparent to white.*/
       document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,'+a+')';
+
       document.getElementById('nav').style.boxShadow = 'none';
       document.getElementById('nav').style.boxShadow = '0px 1px 10px white';
-
       document.getElementById('logoSmall').style.backgroundColor = 'rgb(81%,86%,26%,'+a+')';
 
+      /* Calculate the new color and shadow for the text*/
       var redNew = 100 - 19 * a;
       var greenNew = 100 - 14 * a;
       var blueNew = 100 - 72 * a;
       var shadowNew = 1 - 2 * a;
-
       if (scrollTop >= 0.75*wh) {
         shadowNew = 0;
       }
@@ -94,6 +95,7 @@ function navbarChange() {
         navLinks[i].style.textShadow = '0px 0px '+shadowNew+'px #a2a2a2';
       }
     }
+    /* Styling when scrolled passed front image.*/
     else if (scrollTop >= wh) {
       document.getElementById('nav').style.backgroundColor = 'rgb(255,255,255,1)';
       document.getElementById('nav').style.boxShadow = '0px 2px 2px grey';
@@ -103,6 +105,7 @@ function navbarChange() {
       }
       document.getElementById('logoSmall').style.backgroundColor = 'rgb(81%,86%,26%)'
     }
+    /* Default styling for the navbar on no scroll (top of screen).*/
     else {
       for (i = 0; i < navLinks.length; i++) {
         navLinks[i].style.color = 'white';
@@ -114,7 +117,6 @@ function navbarChange() {
     }
   }
 }
-
 
 var word = '';
 
@@ -131,7 +133,6 @@ function disco(event) {
     word = '';
   }
 }
-
 
 function colorChange() {
   let randomColor = Math.floor(Math.random()*16777215).toString(16);
