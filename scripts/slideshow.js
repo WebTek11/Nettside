d9a310c5f39b4819a65e8eb860ebec3480a15bd0
marhh <37402026+marhh@users.noreply.github.com>
2018-11-05 10:45:27 +0100

    Updated slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -5,13 +5,13 @@ function showSlides() {
 /* Funksjonen viser et slideshow av bilder der det skiftes på hvilket bilde
 som vises hvert 4. sekund. */
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000);
