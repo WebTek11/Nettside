55287e48abf17d9c0829d8bfcd4c431b902b6041
marhh <37402026+marhh@users.noreply.github.com>
2018-11-05 10:48:42 +0100

    Changed slideshow.js

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides();
 
 function showSlides() {
@@ -9,10 +9,10 @@ som vises hvert 4. sekund. */
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000);
+    slideIndex++;
 }
